2037bb0d4f38e445dfd80382a25e9f7a08028838
vishmas <tvishiti@yahoo.com>
2019-02-27 19:03:52 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source = LAPTOP - RHC58KDI; Initial Catalog = Data; User ID = sa; Password = P@55w0rd;");
+            SqlDataAdapter sda = new SqlDataAdapter("select Count(*) from LOGIN where Username = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+
+                this.Hide();
+                Main SS = new Main();
+                SS.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password.");
+            }
+        }
+    }
+}
